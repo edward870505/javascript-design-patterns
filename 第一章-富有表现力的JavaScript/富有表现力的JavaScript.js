584c39b083eb4a4e2d03a3df6f2536b76cbc0f09
edward@Company <edward.huang@foxmail.com>
2019-03-21 15:41:42 +0800

    2019/3/12

---
+++ 第一章-富有表现力的JavaScript/富有表现力的JavaScript.js
@@ -0,0 +1,38 @@
+//page18
+/** Start and stop animations using functions.
+ * 以函数实现一个动画的启动和停止
+ * 这种方法很简单，但无法创建可以保存状态并且具有一些
+ * 仅对其内部状态进行操作的方法的动画对象
+ */
+
+function startAnimation() {
+
+}
+
+function stopAnimation() {
+
+}
+
+/**Anim class
+ * Anim类
+ * 这段代码定义了一个Anim类，并且把两个方法赋给该类的prototype属性
+ */
+var Anim = function () {
+
+};
+
+Anim.prototype.start = function () {
+
+};
+
+Anim.prototype.stop = function () {
+
+};
+
+/**Usage. 
+ * 应用
+*/
+var myAnim = new Anim();
+myAnim.start();
+myAnim.stop();
+
